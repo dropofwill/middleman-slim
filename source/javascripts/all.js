69e3bbd228df73687ab1d9e42ea42f0289685fe2
Will <whp3652@rit.edu>
2014-03-30 21:27:38 -0400

    update javascript setup

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,25 @@
-//= require "lib/jquery.min"
+//= require "lib/jquery/jquery.min"
+
+// JQuery module
+var app = (function($) {
+	
+	//  Private Configurations
+	var config = {
+		aPrivateVar: 'a'
+	};
+
+	// Private functions
+	function aPrivateFunction( $element ) {
+		console.log( $element );
+	}
+
+	return {
+		// Public Functions
+		init: function( $an_element ) {
+			aPrivateFunction( $an_element );
+		}
+	};
+
+})(jQuery);
+
+app.init('body');
